056132b00e8c07af88343cdb4b57e08143b3c866
Rikus <27449440@student.g.nwu.ac.za>
2019-09-03 20:53:34 +0200

    default code removed
    no update per frame, thus code removed

--- 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/CharacterAttributes.cs
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/CharacterAttributes.cs
@@ -2,20 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterAttributes : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-}
 //this class is made to control characters health systems later on.
 public class Health 
 {
