fd8cf0fb6287e81e1be0135d8d451f26d1f9a14f
29578949-NWU <29578949@student.g.nwu.ac.za>
2019-09-10 13:07:59 +0200

    Add first non-interactive entity

---
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/OrbCharacter.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbCharacter : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    public bool isMoving;
+    public float moveTime;
+    private float moveCounter;
+    public float waitTime;
+    private float waitCounter;
+
+    private int MoveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        waitCounter = waitTime;
+        moveCounter = moveTime;
+
+        ChooseDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMoving)
+        {
+            moveCounter -= Time.deltaTime;
+
+            switch(MoveDirection)
+            {
+            case 0:
+                myRigidbody.velocity = new Vector2(0, moveSpeed);
+                break;
+            case 1:
+                myRigidbody.velocity = new Vector2(moveSpeed, 0);
+                break;
+            case 2:
+                myRigidbody.velocity = new Vector2(0, -moveSpeed);
+                break;
+            case 3:
+                myRigidbody.velocity = new Vector2(-moveSpeed, 0);
+                break;
+            }
+
+            if(moveCounter < 0)
+            {
+                isMoving = false;
+                waitCounter = waitTime;
+            }
+        }
+        else
+        {
+            waitCounter -= Time.deltaTime;
+
+            myRigidbody.velocity = Vector2.zero;
+
+            if(waitCounter < 0)
+            {
+                ChooseDirection();
+            }
+        }
+    }
+
+    public void ChooseDirection()
+    {
+        MoveDirection = Random.Range(0, 4);
+        isMoving = true;
+        moveCounter = moveTime;
+    }
+}
