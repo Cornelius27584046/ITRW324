7dd4028e436c29ca48e0aa6adfb9066bda799d83
Rikus <27449440@student.g.nwu.ac.za>
2019-09-26 14:48:30 +0200

    load still buggy
    The load still doesn't function correctly, save, saves all correct elements and their states, load doesn't updat possition or stats correctly

--- 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/SaveProgress.cs
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/SaveProgress.cs
@@ -5,17 +5,12 @@ using System.IO;
 
 public class SaveProgress : MonoBehaviour
 {
-    public GameObject player = new GameObject();
+    [SerializeField]private GameObject player = new GameObject();
 
-    private void Start()
+    private void Awake()
     {
+        SaveSystem.Init();
         player = GameObject.Find("PlayerWitch");
-        SaveObject saveobject = new SaveObject
-        {
-            hadArmor = CharacterAttributes.PlayerArmor.getArmorOn(),
-            hadHealth = CharacterAttributes.PlayerHealth.getMaxHealth(),
-            hadArmorType = CharacterAttributes.PlayerArmor.ArmorType,
-        };
     }
 
     //update method to call save method once key pressed
@@ -38,32 +33,33 @@ public class SaveProgress : MonoBehaviour
         Vector3 playerposition = player.transform.position;
         SaveObject saveobject = new SaveObject
         {
-            hadArmor = CharacterAttributes.PlayerArmor.getArmorOn(),
+            hadArmor = CharacterAttributes.PlayerArmor.getArmorStat(),
+            hadMaxA = CharacterAttributes.PlayerArmor.getArmorOn(),
             hadArmorType = CharacterAttributes.PlayerArmor.ArmorType,
+            hadMaxH = CharacterAttributes.PlayerHealth.getMaxHealth(),
             hadHealth = CharacterAttributes.PlayerHealth.getHealth(),
             playerPos = playerposition,
         };
 
         string datatosave = JsonUtility.ToJson(saveobject);
-
-        File.WriteAllText(Application.dataPath + "/save.txt", datatosave);
-        Debug.Log("Saved!");
+        SaveSystem.Save(datatosave);
     }
 
     //method to load the players data from a previous session
     private void Load()
     {
-        if (File.Exists(Application.dataPath + "/save.txt"))
+        string saveString = SaveSystem.Load();
+        if (saveString != null)
         {
-            string savestring = File.ReadAllText(Application.dataPath + "/save.txt");
-
-            SaveObject saveObject = JsonUtility.FromJson<SaveObject>(savestring);
+            SaveObject saveObject = JsonUtility.FromJson<SaveObject>(saveString);
 
             //below line sets the players position & Does not do a thing
             player.gameObject.transform.position.Set(saveObject.playerPos.x, saveObject.playerPos.y, saveObject.playerPos.z);
             //below sets the attributes & works!!!
+            CharacterAttributes.PlayerArmor.getArmorOn();
             CharacterAttributes.PlayerHealth.setHealth(0, saveObject.hadHealth);
             CharacterAttributes.PlayerArmor.setArmorStat(saveObject.hadArmorType, 0, saveObject.hadArmor);
+
             Debug.Log("Loaded!");
         }
         else
@@ -75,6 +71,8 @@ public class SaveProgress : MonoBehaviour
     //class to set which attributes must be saved.
     private class SaveObject
         {
+        public int hadMaxH = -1;
+        public int hadMaxA = -1;
         public int hadHealth = -1;
         public int hadArmor = -1;
         public string hadArmorType = "doal..."; //its load spelled backwards... there to know load failed.
---
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class SaveSystem
+{
+    private static readonly string SAVE_FOLDER = Application.dataPath + "/Saves/";
+
+    public static void Init()
+    {
+
+        if (!Directory.Exists(SAVE_FOLDER))
+        {
+            Directory.CreateDirectory(SAVE_FOLDER);
+        }
+    }
+
+    public static void Save(string saveString)
+    {
+        File.WriteAllText(SAVE_FOLDER + "save.txt", saveString);
+        Debug.Log("Saved!");
+    }
+
+    public static string Load()
+    {
+        if (File.Exists(SAVE_FOLDER + "/save.txt"))
+        {
+            string savestring = File.ReadAllText(SAVE_FOLDER + "/save.txt");
+            Debug.Log("Loaded!");
+            return savestring;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
