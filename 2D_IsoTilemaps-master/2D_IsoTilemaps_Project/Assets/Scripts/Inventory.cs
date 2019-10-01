2b635df9a5668e8898fa08e879faf043fed6741b
Rikus <27449440@student.g.nwu.ac.za>
2019-10-01 16:20:40 +0200

    Inventory Layout added
    the visual aspect is showing for the players backpack

---
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(30, 100, 100, 100),"Backpack");
+        //armor can either be destroyed on 0, meaning its left 0 and no repairs can be made, or code can be reverted to ensure the armor is always on, even on 0 and can be restored from that point.
+    }
+}
