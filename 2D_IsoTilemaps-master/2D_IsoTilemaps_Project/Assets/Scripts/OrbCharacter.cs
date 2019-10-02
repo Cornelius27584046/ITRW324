eecf6dba6e0f66c60e98799d55b969bea78e0e23
29578949-NWU <29578949@student.g.nwu.ac.za>
2019-10-02 11:32:08 +0200

    Orb movement randomized

--- 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/OrbCharacter.cs
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/OrbCharacter.cs
@@ -18,8 +18,11 @@ public class OrbCharacter : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody2D>();
 
-        waitCounter = waitTime;
-        moveCounter = moveTime;
+        //waitCounter = waitTime;
+        //moveCounter = moveTime;
+
+        waitCounter = Random.Range(waitTime * 0.75f, waitTime * 1.25f);
+        moveCounter = Random.Range(moveTime * 0.75f, waitTime * 1.25f);
 
         ChooseDirection();
     }
@@ -49,7 +52,9 @@ public class OrbCharacter : MonoBehaviour
             if(moveCounter < 0)
             {
                 isMoving = false;
-                waitCounter = waitTime;
+                //waitCounter = waitTime;
+                waitCounter = Random.Range(waitTime * 0.75f, waitTime * 1.25f);
+
             }
         }
         else
@@ -67,6 +72,7 @@ public class OrbCharacter : MonoBehaviour
     {
         MoveDirection = Random.Range(0, 4);
         isMoving = true;
-        moveCounter = moveTime;
+        //moveCounter = moveTime;
+        moveCounter = Random.Range(moveTime * 0.75f, waitTime * 1.25f);
     }
 }
