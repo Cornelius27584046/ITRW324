9397ca84f50c788770dfe34c8b94e8ef93d89b05
29578949-NWU <29578949@student.g.nwu.ac.za>
2019-10-02 15:18:16 +0200

    Initial player health check, death results in level reset, no score is reset yet

---
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+
+    public int damageDealt;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealth>().HurtPlayer(damageDealt);
+        }
+    }
+}
---
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int playerMaxHealth;
+    public int playerCurrentHealth;
+
+    public float waitToReload;
+    public bool reloading;
+    private GameObject thePlayer;
+
+    void Start()
+    {
+        playerCurrentHealth = playerMaxHealth;
+    }
+
+    void Update()
+    {
+        if(playerCurrentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+            reloading = true;
+            //thePlayer = other.gameObject;
+        }
+
+    if(reloading)
+        {
+            waitToReload -= Time.deltaTime;
+            if(waitToReload < 0)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+                //thePlayer.SetActive(true);
+            }
+        }
+    }
+
+    public void HurtPlayer(int damageDealt)
+    {
+        playerCurrentHealth -= damageDealt;
+    }
+
+    public void SetMaxHealth()
+    {
+        playerCurrentHealth = playerMaxHealth;
+    }
+}
