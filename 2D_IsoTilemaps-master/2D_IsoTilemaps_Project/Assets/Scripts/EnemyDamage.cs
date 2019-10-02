7638f546828f648a69fc24bdc57506925afce274
Rikus <27449440@student.g.nwu.ac.za>
2019-10-02 14:47:25 +0200

    Reworked SwordBox
    Changed the box collider of the sword to not be so big

--- 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/EnemyDamage.cs
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/EnemyDamage.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 { 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Enemy")
