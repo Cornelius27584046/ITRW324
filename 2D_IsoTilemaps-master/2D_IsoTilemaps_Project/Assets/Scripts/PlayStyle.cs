65e5748b51e36b0167a210861c0d95c8068234b5
Rikus <27449440@student.g.nwu.ac.za>
2019-10-23 17:00:02 +0200

    Device Detection
    A script has been implemented to determine what input methods the user will be using, desktop or mobile, in any case, the script allocates character movement accordingly.

--- 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/PlayStyle.cs
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/PlayStyle.cs
@@ -4,63 +4,58 @@ using UnityEngine;
 
 public class PlayStyle : MonoBehaviour
 {
-    private float movementSpeed = 1f;
+    private float movementSpeed = 3f;
     private Rigidbody2D rb;
     private IsometricCharacterRenderer isoRenderer;
-    public PlayStyle()
+    private Vector2 inputVector;
+    private float ChardirX;
+    private float ChardirY;
+    private float horizontalInput;
+    private float verticalInput;
+
+    void Awake()
     {
-        Debug.Log("Determining device");
-        if (SystemInfo.deviceType == DeviceType.Handheld)
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        ChardirX = Input.acceleration.x * movementSpeed;
+        ChardirY = Input.acceleration.y * movementSpeed;
+        rb = GetComponent<Rigidbody2D>();
+        isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+        CharacterAttributes.PlayerHealth.setHealth(0, 100);
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+        CharacterAttributes.PlayerHealth.setHealth(0, 100);
+    }
+    void FixedUpdate()
+    {
+        Vector2 currentPos = rb.position;
+        determineDevice(); //calldevice method
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * movementSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        isoRenderer.SetDirection(movement);
+        rb.MovePosition(newPos);
+    }
+    public void determineDevice()
+    {
+        if (SystemInfo.deviceType == DeviceType.Desktop)
         {
-            void Awake()
-            {
-                rb = GetComponent<Rigidbody2D>();
-                isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
-                CharacterAttributes.PlayerHealth.setHealth(0, 100);
-            }
-         // Update is called once per frame
-            void FixedUpdate()
-            {
-                Vector2 currentPos = rb.position;
-                float horizontalInput = Input.GetAxis("Horizontal");
-                float verticalInput = Input.GetAxis("Vertical");
-                Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
-                inputVector = Vector2.ClampMagnitude(inputVector, 1);
-                Vector2 movement = inputVector * movementSpeed;
-                Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-                isoRenderer.SetDirection(movement);
-                rb.MovePosition(newPos);
-            }
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            inputVector = new Vector2(horizontalInput, verticalInput);
+            rb.velocity = new Vector2(ChardirX, ChardirY);
+            //transform.position = new Vector2(Mathf.Clamp(transform.position.x, -23f, 23f), Mathf.Clamp(transform.position.y, -12, 13));
         }
         if (SystemInfo.deviceType == DeviceType.Handheld)
         {
-            float ChardirX;
-            float ChardirY;
-
-            void Start()
-            {
-                rb = GetComponent<Rigidbody2D>();
-                isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
-                CharacterAttributes.PlayerHealth.setHealth(0, 100);
-            }
-
-            void Update()
-            {
-                ChardirX = Input.acceleration.x * movementSpeed;
-                ChardirY = Input.acceleration.y * movementSpeed;
-                transform.position = new Vector2(Mathf.Clamp(transform.position.x, -23f, 23f), Mathf.Clamp(transform.position.y, -12, 13));
-            }
-            void FixedUpdate()
-            {
-                rb.velocity = new Vector2(ChardirX, ChardirY);
-                Vector2 currentPos = rb.position;
-                Vector2 inputVector = new Vector2(ChardirX, ChardirY);
-                inputVector = Vector2.ClampMagnitude(inputVector, 1);
-                Vector2 movement = inputVector * movementSpeed;
-                Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-                isoRenderer.SetDirection(movement);
-                rb.MovePosition(newPos);
-            }
+            ChardirX = Input.acceleration.x * movementSpeed;
+            ChardirY = Input.acceleration.y * movementSpeed;
+            inputVector = new Vector2(ChardirX, ChardirY);
+            rb.velocity = new Vector2(ChardirX, ChardirY);
+            transform.position = new Vector2(Mathf.Clamp(transform.position.x, -23f, 23f), Mathf.Clamp(transform.position.y, -12, 13));
         }
     }
 }
