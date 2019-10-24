cf160a034e5a5f8e4c8a15b6f266cf6f2b2ff825
29578949-NWU <29578949@student.g.nwu.ac.za>
2019-10-24 22:35:32 +0200

    Expand boss area, add boss AI

---
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/BossController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    private Animator myAnim;
+    private Transform target;
+
+    public float speed;
+    public float minRange;
+    public float maxRange;
+
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        target = FindObjectOfType<IsometricCharacterRenderer>().transform;
+    }
+    
+    void Update()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= maxRange && Vector3.Distance(target.position, transform.position) >= minRange)
+        {
+            FollowPlayer();
+        }
+    }
+
+    public void FollowPlayer()
+    {
+        myAnim.SetBool("isMoving", true);
+        myAnim.SetFloat("moveX", (target.position.x - transform.position.x));
+        myAnim.SetFloat("moveY", (target.position.y - transform.position.y));
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
