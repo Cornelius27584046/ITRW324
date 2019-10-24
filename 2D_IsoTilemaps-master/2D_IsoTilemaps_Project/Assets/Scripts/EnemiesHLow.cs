becb9bb4475e28a7975c2b99c2ecdc7573b2ab34
Rikus <27449440@student.g.nwu.ac.za>
2019-10-24 16:47:24 +0200

    Creating Enemy health systems
    # health stages for the enemies are being implemented, one for low level enemies, one foe moderate enemies, and one for high level eneimies such as bosses.

---
+++ 2D_IsoTilemaps-master/2D_IsoTilemaps_Project/Assets/Scripts/EnemiesHLow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesHLow : MonoBehaviour
+{
+    private int health = 30;
+    private int armor = 0;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Sword")
+        {
+            setHealth(10);
+            Debug.Log(health);
+        }
+    }
+    private void setHealth(int damage)
+    {
+        health -= damage;
+    }
+}
